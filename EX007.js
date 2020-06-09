16c50ee6c8956257eddcbceadccf866b511fcf9c
HugoOliveiraSoares <hugoliveira.soares@gmail.com>
2020-06-09 22:43:30 +0000

    first commit

---
+++ EX007.js
@@ -0,0 +1,8 @@
+var vel = 12
+console.log(`A velociade do seu carro é ${vel}km/h`)
+if (vel > 60)
+{
+    console.log('Você passou a velocidade permitida. MULTADO!')
+}
+
+console.log('Dirija sempre usando cinto de segurança!')
